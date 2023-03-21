97c7d1034267aa37800a015c10e79d7ce9917894
superchen <chensuper0202@gmail.com>
2023-03-21 17:40:38 +0800

    revise the navbar style

--- src/components/shared/NavBar/NavBar.tsx
+++ src/components/shared/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import style from './NavBar.module.scss';
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from '@/slice/hooks';
 import { getMe } from "@/slice/authSlice";
 
@@ -13,35 +13,33 @@ const NavBar: React.FC<NavBarProps> = (props) => {
 
   useEffect(() => {
     dispatch(getMe());
+    console.log(user);
   }, [dispatch])
 
   return (
-  
-  <div className={style["header-container"]}>
+    <header className="sticky top-0 bg-white border-b-[1px] border-gray-300">
 
-    <div className={style["left-navbar"]}>
-      
-      <h1 className={style.brand}>
-        <NavLink to="/" className={style.nav}>
-          React 部落格
-        </NavLink>
-      </h1>
+      <div className="flex justify-between items-center
+                      h-[60px] px-2 box-border
+                      lg:max-w-[1024px] lg:mx-auto lg:px-0"
+      >
+        {/* Left Side */}
+        <div className="w-[100px] cursor-pointer">        
+          <h1 className="m-0">
+            <Link to="/" className={style.nav}>部落格</Link>
+          </h1>
+        </div>
 
-      <ul className={style["navbar-list"]}>
-        <NavLink to="/" className={style.nav}>首頁</NavLink>
-        <NavLink to="/message-board" className={style.nav}>發布文章</NavLink>
-      </ul>
-
-    </div>
-
-
-    <ul className={style["navbar-list"]}>
-        <NavLink to="/login" className={style.nav}>
-        { user ? <p>{user}</p>:<p>登入</p>}  
-        </NavLink>
-    </ul>
-    
-  </div>
+        {/* Right Side */}
+        <ul className="flex items-center">
+          <NavLink to="/" className={style.nav}>首頁</NavLink>
+          <NavLink to="/message-board" className={style.nav}>發布文章</NavLink>
+          <NavLink to="/login" className={style.nav}>
+            { user ? <p>{user}</p> : <p>登入</p> }   
+          </NavLink>
+        </ul>
+      </div>
+    </header>
   )
 };
 
